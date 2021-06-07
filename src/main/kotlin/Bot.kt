29c59d7bf8dfec10b5e0f17150bbd434afdd83d8
Michael Langer <michael.langer@check24.de>
2021-06-07 11:43:09 +0200

    [main] tried using socket mode

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,6 +1,6 @@
 import com.slack.api.bolt.App
 import com.slack.api.bolt.AppConfig
-import com.slack.api.bolt.jetty.SlackAppServer
+import com.slack.api.bolt.socket_mode.SocketModeApp
 import com.slack.api.model.block.Blocks.asBlocks
 import com.slack.api.model.block.Blocks.section
 import com.slack.api.model.block.composition.BlockCompositions.markdownText
@@ -13,6 +13,8 @@ fun main() {
     // https://github.com/slackapi/java-slack-sdk/blob/master/bolt-kotlin-examples/src/main/kotlin/examples/reply/app.kt
     // https://gist.github.com/seratch/6ab9139d15ad33c9b1e149327b5f14fa
     // https://slack.dev/java-slack-sdk/guides/bolt-basics
+    // https://slack.dev/java-slack-sdk/guides/getting-started-with-bolt
+    // https://slack.dev/java-slack-sdk/guides/getting-started-with-bolt-socket-mode
 
     System.setProperty("org.slf4j.simpleLogger.log.com.slack.api", "debug")
     System.setProperty("org.slf4j.simpleLogger.log.notion.api", "debug")
@@ -39,6 +41,10 @@ fun main() {
         ctx.ack()
     }
 
-    val server = SlackAppServer(app)
-    server.start() // http://localhost:3000/slack/events
+    app.message("test 123") { event , ctx  ->
+        ctx.say("<@${event.event.user}> What's up? message")
+        ctx.ack()
+    }
+
+    SocketModeApp(app).start()
 }
