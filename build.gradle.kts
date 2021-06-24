a2c6c7cdc80ef04a4093a055fc50f4ec8c77b85e
Michael Langer <mail@michaellanger.me>
2021-06-24 22:54:06 +0200

    moved to refreshVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm")
     id("application")
-    id("com.github.ben-manes.versions") version "0.39.0" // ./gradlew dependencyUpdates -Drevision=release
 }
 
 repositories {
@@ -16,15 +15,10 @@ application {
 }
 
 dependencies {
-    val slack_bolt_version = "1.8.1"
-    val javax_websocket_version = "1.1"
-    val tyrus_version = "1.17"
-    val slf4j_version = "1.7.31"
-
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.slack.api:bolt-socket-mode:$slack_bolt_version")
-    implementation("javax.websocket:javax.websocket-api:$javax_websocket_version")
-    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:$tyrus_version")
-    implementation("org.slf4j:slf4j-simple:$slf4j_version")
+    implementation("com.slack.api:bolt-socket-mode:_")
+    implementation("javax.websocket:javax.websocket-api:_")
+    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:_")
+    implementation("org.slf4j:slf4j-simple:_")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
-rootProject.name = "hh-app-slack-bot"
+rootProject.name = "hh-app-bot"
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.10.1" // ./gradlew refreshVersions
+}
 
