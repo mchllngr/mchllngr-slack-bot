8d7c4982ec361bc37e70fb51b5e5c7fc77bf3e56
Michael Langer <mail@michaellanger.me>
2021-08-07 21:52:16 +0200

    updated dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "hh-app-bot"
 
 plugins {
-    id("de.fayard.refreshVersions") version "0.10.1" // ./gradlew refreshVersions
+    id("de.fayard.refreshVersions") version "0.11.0" // ./gradlew refreshVersions
 }
 
